bda82c8dc68edc7fecfc63da2312ffd1c0b6e2a7
Juri Zaburunov <juri.zaburunov@gmail.com>
2020-08-06 13:36:50 +0300

    feat: Add server logging middleware

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@
 // Handle login route - controller router
 // Handle route /pizzas/all - create controller class
 import express from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message: string) => {
+        console.log("info", message);
+      },
+    },
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listen on port ${PORT}`);
